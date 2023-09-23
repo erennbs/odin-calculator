272d675d0867f21e5a710fd6a3b5aaef8987fb1e
Eren Bas <erenbas3453@gmail.com>
2023-09-23 20:16:39 +0300

    First Commit
    * Add basic layout of the calculator
    * Add basic math functions

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b === 0) return;
+
+    return a / b;
+}
+
+function operate(num1, num2, operator) {
+    if (operator === '+') return add(num1, num2);
+    if (operator === '-') return subtract(num1, num2);
+    if (operator === '*') return multiply(num1, num2);
+    if (operator === '/') return divide(num1, num2);
+}
