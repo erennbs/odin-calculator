3341a152bdb4d40c4db94d9a62d245f72a167db9
Eren Bas <erenbas3453@gmail.com>
2023-09-24 20:06:19 +0300

    Add functionality to buttons
    * All buttons are functional except "."
    * Operations are visible on screen

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
+const numberButtons = document.querySelectorAll('button.number');
+const operatorButtons = document.querySelectorAll('button.operator');
+const otherButoons = document.querySelectorAll('button.other');
+const calculationScreen = document.querySelector('.calculation');
+const answerScreen = document.querySelector('.answer');
+
+numberButtons.forEach((btn) => btn.addEventListener('click', onNumberClick));
+operatorButtons.forEach((btn) => btn.addEventListener('click', onOperatorClick));
+otherButoons.forEach((btn) => btn.addEventListener('click', onOthersClick));
+
+let num1 = '';
+let num2 = '';
+let operator = null;
+
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return +a - +b;
 }
 
 function multiply(a, b) {
-    return a * b;
+    return +a * +b;
 }
 
 function divide(a, b) {
-    if (b === 0) return;
+    if (+b === 0) return;
 
-    return a / b;
+    return +a / +b;
 }
 
 function operate(num1, num2, operator) {
@@ -21,4 +35,82 @@ function operate(num1, num2, operator) {
     if (operator === '-') return subtract(num1, num2);
     if (operator === '*') return multiply(num1, num2);
     if (operator === '/') return divide(num1, num2);
+}
+
+function onNumberClick(e) {
+    if (!operator) {
+        // We are entering the first number
+        num1 += e.target.value;
+        updateAnswerDiv(num1);
+    } else {
+        // We are entering the second number
+        num2 += e.target.value;
+        updateAnswerDiv(num2);
+    }
+}
+
+function onOperatorClick(e) {
+    if (!operator) {
+        operator = e.target.value;
+    } else {
+        result = operate(num1, num2, operator);
+        num1 = result.toString();
+        num2 = '';
+        operator = e.target.value;
+        updateCalculationDiv(`${num1} ${operator} `);
+        updateAnswerDiv(result);
+    }
+}
+
+function onOthersClick(e) {
+    switch (e.target.value) {
+        case 'ac':
+            resetCalculator();
+            break;
+        case 'c':
+            removeLastDigit();
+            break;
+        case '=':
+            calculate();
+            break;    
+        default:
+            break;
+    }
+}
+
+function resetCalculator() {
+    num1 = '';
+    num2 = '';
+    operator = null;
+    updateCalculationDiv('');
+    updateAnswerDiv('');
+}
+
+function removeLastDigit() {
+    if (!operator) {
+        num1 = num1.slice(0, -1);
+        updateAnswerDiv(num1);
+    } else {
+        num2 = num2.slice(0, -1);
+        updateAnswerDiv(num2);
+    }
+}
+
+function calculate() {
+    if (!operator || !num1 || !num2) return;
+
+    result = operate(num1, num2, operator);
+    updateCalculationDiv(`${num1} ${operator} ${num2} =`);
+    updateAnswerDiv(result);
+    num1 = result.toString();
+    num2 = '';
+    operator = null;
+}
+
+function updateCalculationDiv(text) {
+    calculationScreen.textContent = text;
+}
+
+function updateAnswerDiv(text) {
+    answerScreen.textContent = text;
 }
